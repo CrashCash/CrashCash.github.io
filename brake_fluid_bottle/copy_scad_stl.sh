2eaefeffef1d565c0412f84f3df3dacfb6ebc072
Gene Cash <genecash@fastmail.com>
2024-01-06 10:30:48 -0500

    Added script to copy brake fluid 3D printer files

---
+++ brake_fluid_bottle/copy_scad_stl.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cp /storage/3D_printer/my_designs/brake_fluid/master_cylinder_plate/brake_fluid_plate.scad .
+cp /storage/3D_printer/my_designs/brake_fluid/master_cylinder_plate/brake_fluid_plate.stl .

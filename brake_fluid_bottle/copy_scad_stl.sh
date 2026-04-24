2c5c7c31cabe81a86d4d88d20f26bc9415315302
Gene Cash <genecash@fastmail.com>
2026-04-24 18:40:31 -0400

    Rewrote brake fluid jar page

--- brake_fluid_bottle/copy_scad_stl.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cp /storage/3D_printer/my_designs/brake_fluid/master_cylinder_plate/brake_fluid_plate.scad .
-cp /storage/3D_printer/my_designs/brake_fluid/master_cylinder_plate/brake_fluid_plate.stl .
-
-cp /storage/3D_printer/my_designs/brake_fluid/ss9_front/brake_fluid-ss9_front.scad .
-cp /storage/3D_printer/my_designs/brake_fluid/ss9_front/brake_fluid-ss9_front.stl .
